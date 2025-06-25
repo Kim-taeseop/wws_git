00a2400df6355ae17d2df67185f56ed07f979570
Kim tae-seop <kts3445@naver.com>
2025-06-25 18:24:18 +0900

    feat : Repository 기능 추가 엔티티 추가, 단건 조회, 전체 조회 추가

--- src/main/java/com/tabby/wws/repository/ItemsRepository.java
+++ src/main/java/com/tabby/wws/repository/ItemsRepository.java
@@ -1,12 +1,35 @@
 package com.tabby.wws.repository;
 
+import com.tabby.wws.entity.Items;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class ItemsRepository {
 
     private final EntityManager em;
+
+    // 아이템 등회
+    public void save(Items items) {
+        if(items.getId() == null){
+            em.persist(items);
+        } else{
+            em.merge(items);
+        }
+    }
+
+    // 아이템 단건 조회
+    public Items findOne(Long id){
+        return em.find(Items.class, id);
+    }
+
+    // 아이템 리스트 조회
+    public List<Items> findAll() {
+        return em.createQuery( "select i from Items i" , Items.class)
+                .getResultList();
+    }
 }
